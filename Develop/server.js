f3955f80627894a3c0d2ad66f55b80f1fe618f1d
Rodolfo Diaz <rodolfodzr@gmail.com>
2020-11-22 00:52:42 -0500

    getting the Server to work

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,10 @@
 // Declaring dependencies 
-var express=require("express");
+const express=require("express");
+const fs=require("fs");
+const util=require("util");
 var path=require("path");
-var db=require("./db/db")
+var db=require("./db/db");
+const { promises } = require("dns");
 
 //Setting up Express 
 //==========================
@@ -12,26 +15,31 @@ var PORT= 3000;
 //==========================================
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
+
+const readFileAsync=util.promisify(fs.readFile);
+const writeFileAsync=util.promisify(fs.writeFile);
+
 
  //routes
  //================================= HTML ROUTES
+
  app.get("/notes",function(req,res){
-     res.sendFile(path.join(__dirname,"/public/","notes.html"));
+     res.sendFile(path.join(__dirname,"public","notes.html"));
+ });
+ app.get("/",function(req,res){
+     res.sendFile(path.join(__dirname,"public","index.html"));
  });
 
- app.get("*",function(req,res){
-     res.sendFile(path.join(__dirname,"/public/","index.html"));
- });
  //====   API Routes===
  app.get("/api/notes", function(req, res){
      return res.json(db);
  });
  // post to receive a new note from body and add it to the json file
- app.post("/api/notes", function(req, resp){
+ app.post("/api/notes", function(req, res){
      db.push(req.body);
      res.json(true);
  }) 
-
  //===========
  //   Port listener standard 
  app.listen(PORT,function(){
