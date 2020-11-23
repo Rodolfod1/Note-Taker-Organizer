5a3051270c459344587e4fe75e2275eaa99b0b83
Rodolfo Diaz <rodolfodzr@gmail.com>
2020-11-22 22:10:02 -0500

    integrating the methods post and delete

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,4 @@
-// Declaring dependencies 
+// Declaring dependencies
 const express=require("express");
 const fs=require("fs");
 const util=require("util");
@@ -8,8 +8,9 @@ var path=require("path");
 var db=require("./db/db");
 const { promises } = require("dns");
 const { json } = require("express");
+const { log } = require("console");
 
-//Setting up Express 
+//Setting up Express
 //==========================
 var app= express();
 var PORT= 3000;
@@ -20,12 +21,12 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
-const readFileAsync=util.promisify(fs.readFile);
-const writeFileAsync=util.promisify(fs.writeFile);
+// const readFile=util.promisify(fs.readFile);
+// const writeFile=util.promisify(fs.writeFile);
 
 
  //routes
- //================================= HTML ROUTES
+ //= HTML ROUTES
 
  app.get("/notes",function(req,res){
      res.sendFile(path.join(__dirname,"public","notes.html"));
@@ -35,36 +36,51 @@ const writeFileAsync=util.promisify(fs.writeFile);
  });
 
  //====   API Routes===
+ //  GET route 
  app.get("/api/notes", function(req, res){
-     fs.readFile(__dirname + "/db/db.json","UTF8", function(err,dat){
-         if(err) throw err;
-         var Notes= JSON.parse(dat);
-         res.json(Notes);
-     })
- });
+         res.json(db);
+      });
 
- // post to receive a new note from body and add it to the json file
- app.post("/api/notes", async (req,res) => {
-    var Notes= await readFileAsync("./db/db.json", "UTF8");
-    Notes=JSON.parse(Notes);
-    req.body.id= uuidv4();
-    Notes.push(req.body);
+ // POST route to receive a new note from body and add it to the json file
+app.post("/api/notes", function(req, res){
+      // using UUID to get the Note from the body on an array 
+       req.body.id = uuidv4();
+       // this variable is to make the code more readable 
+       var Note=req.body
+    //pushing the new note to the JSON file on memory          
+       db.push(Note);
+    // write db JSON file from mem to the file 
+     fs.writeFile("./db/db.json",JSON.stringify(db),function(err){
+         if (err) throw err;
+         //if there is no error then send response 
+         res.json(db);
+     });
+  })
 
-    fs.writeFile(__dirname + "./db/db.json", JSON.stringify(Notes));
-    res.json(Notes[Notes.length-1]);
- }
+ // DELETE Note Method
+ app.delete("/api/notes",function(req,res){
+    // Getting the ID from request  
+    var Id = req.params.id;
+    // uding ID on loop at the dbJSON file 
+    for(i=0; i<db.length; i++){
+        // selecting the id and splicing the element
+        if(Id=== db[i].id){
+            db.splice(i,1);
+          };
+        }
+    //writing the new db from the mem to the file     
+    fs.writeFile("./db/db.json",JSON.stringify(db),function(err){
+            if (err) throw err;
+            //if there is no error then send response 
+            res.json(db);
+     });
+ })
  
  
- 
- 
- 
- 
- function(req, res){
-     db.push(req.body);
-     res.json(true);
- }) 
+
+
  //===========
- //   Port listener standard 
+ //   Port listener standard
  app.listen(PORT,function(){
     console.log("App listening on PORT: " + PORT);
  })
